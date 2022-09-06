542d807f4eb7bc18b4cd266dcbddedc93fce9d36
Dawid-Koralewski <dasiokk@gmail.com>
2022-09-06 19:54:49 +0200

    Divide KsiazkaAdresowa to smaller classes

---
+++ MetodyPomocnicze.h
@@ -0,0 +1,19 @@
+#ifndef METODYPOMOCNICZE_H
+#define METODYPOMOCNICZE_H
+
+#include <iostream>
+#include <sstream>
+#include <windows.h>
+#include <fstream>
+
+using namespace std;
+
+class MetodyPomocnicze
+{
+
+public:
+    static string konwerjsaIntNaString(int liczba);
+    static string wczytajLinie();
+};
+
+#endif
---
+++ UzytkownikManedzer.cpp
@@ -0,0 +1,62 @@
+#include "UzytkownikMenedzer.h"
+
+
+Uzytkownik UzytkownikMenedzer::podajDaneNowegoUzytkownika()
+{
+    Uzytkownik uzytkownik;
+
+    uzytkownik.ustawID(pobierzIdNowegoUzytkownika());
+
+    do
+    {
+        cout << endl << "Podaj login: ";
+        uzytkownik.ustawLogin(MetodyPomocnicze::wczytajLinie());
+    } while (czyIstniejeLogin(uzytkownik.pobierzLogin()) == true);
+
+    cout << "Podaj haslo: ";
+    uzytkownik.ustawHaslo(MetodyPomocnicze::wczytajLinie());
+
+    return uzytkownik;
+}
+
+int UzytkownikMenedzer::pobierzIdNowegoUzytkownika()
+{
+    if (uzytkownicy.empty() == true)
+        return 1;
+    else
+        return uzytkownicy.back().pobierzId() + 1;
+}
+
+bool UzytkownikMenedzer::czyIstniejeLogin(string login)
+{
+    for (int i = 0; i < uzytkownicy.size(); i++)
+    {
+        if (uzytkownicy[i].pobierzLogin() == login)
+        {
+            cout << endl << "Istnieje uzytkownik o takim loginie." << endl;
+            return true;
+        }
+    }
+    return false;
+}
+
+void UzytkownikMenedzer::rejestracjaUzytkownika()
+{
+    Uzytkownik uzytkownik = podajDaneNowegoUzytkownika();
+
+    uzytkownicy.push_back(uzytkownik);
+    plikZUzytkownikami.dopiszUzytkownikaDoPliku(uzytkownik);
+
+    cout << "Konto zalozono pomyslnie" << endl << endl;
+    system("pause");
+}
+
+void UzytkownikMenedzer::wypiszWszystkichUzytkownikow()
+{
+    for (int i = 0; i < uzytkownicy.size(); i++)
+    {
+        cout << endl << uzytkownicy[i].pobierzId();
+        cout << endl << uzytkownicy[i].pobierzLogin();
+        cout << endl << uzytkownicy[i].pobierzHaslo();
+    }
+}
