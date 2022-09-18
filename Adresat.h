49811eb2a29aa5896eeadffccb09afe31899f9b0
Dawid-Koralewski <dasiokk@gmail.com>
2022-09-19 00:13:19 +0200

    Organizational work - preparation of files for a implementation of /'Adresat/' feature

---
+++ Adresat.h
@@ -0,0 +1,24 @@
+#ifndef ADRESAT_H
+#define ADRESAT_H
+
+#include <iostream>
+
+using namespace std;
+
+class Adresat
+{
+//    int id = 0;
+//    string login;
+//    string haslo;
+//
+//public:
+//    void ustawID(int noweId);
+//    void ustawLogin(string nowyLogin);
+//    void ustawHaslo(string noweHaslo);
+//
+//    int pobierzId();
+//    string pobierzLogin();
+//    string pobierzHaslo();
+};
+
+#endif
---
+++ AdresatMenedzer.h
@@ -0,0 +1,34 @@
+#ifndef ADRESATMENEDZER_H
+#define ADRESATMENEDZER_H
+
+#include <iostream>
+#include <vector>
+#include <windows.h>
+#include <fstream>
+#include <sstream>
+
+#include "Adresat.h"
+#include "PlikZAdresatami.h"
+
+using namespace std;
+
+class AdresatMenedzer
+{
+//    vector <Uzytkownik> uzytkownicy;
+//    PlikZUzytkownikami plikZUzytkownikami;
+//
+//    Uzytkownik podajDaneNowegoUzytkownika();
+//    Uzytkownik zalogowanyUzytkownik;
+//    int pobierzIdNowegoUzytkownika();
+//    bool czyIstniejeLogin(string login);
+//
+//public:
+//    UzytkownikMenedzer(string nazwaPlikuZUzytkownikami) : plikZUzytkownikami(nazwaPlikuZUzytkownikami) {};
+//    void rejestracjaUzytkownika();
+//    void wypiszWszystkichUzytkownikow();
+//    void wczytajUzytkownikowZPliku();
+//    Uzytkownik logowanieUzytkownika();
+//    vector <Uzytkownik> pobierzUzytkownikow();
+};
+
+#endif
